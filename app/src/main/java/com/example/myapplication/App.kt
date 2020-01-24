6eb55c9fba0f3fa05b626f76ce44db78b7210d73
hanieh nikjoo <hnik22@gmailo.com>
2020-01-24 11:35:31 +0330

    Added koin dependency and Added App,ViewModelModule

---
+++ app/src/main/java/com/example/myapplication/App.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import android.app.Application
+import com.example.myapplication.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+open class App : Application() {
+
+    companion object {
+        var instance: App? = null
+            private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        instance = this
+
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(listOf( viewModelModule ))
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/di/ViewModelModule.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.di
+
+
+import org.koin.dsl.module
+
+val viewModelModule = module {
+        //        viewModel { UserRegistrationViewModel() }
+}
