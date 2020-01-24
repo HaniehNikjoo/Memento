f61c4d405f36d0bc4c7a1db9ec754cac795dd95a
hanieh nikjoo <hnik22@gmailo.com>
2020-01-24 12:18:48 +0330

    Added AppPreferencesManager

---
+++ app/src/main/java/com/example/myapplication/prefs/AppPreferencesManager.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication.prefs
+
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+
+class AppPreferencesManager : PreferencesManager {
+    private var mPrefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(CardServices.context)
+
+    override fun isLogin(): Boolean {
+        return mPrefs.getBoolean(PREF_KEY_IS_LOGIN, false)
+    }
+    override fun setLogin(isLogging: Boolean){
+        mPrefs.edit().putBoolean(PREF_KEY_IS_LOGIN, isLogging).apply()
+    }
+    override fun saveMobile(): String {
+        return mPrefs.getString(PREF_KEY_MOBILE, "")!!
+    }
+    override fun setMobile(mobile: String) {
+        mPrefs.edit().putString(PREF_KEY_MOBILE, mobile).apply()
+    }
+    override fun getToken(): String {
+        return mPrefs.getString(PREF_KEY_TOKEN, "")!!
+    }
+    override fun saveToken(token: String) {
+        mPrefs.edit().putString(PREF_KEY_TOKEN, token).apply()
+    }
+    companion object {
+        private const val PREF_KEY_IS_LOGIN = "PREF_KEY_IS_LOGIN"
+        private const val PREF_KEY_MOBILE = "PREF_KEY_MOBILE"
+        private const val PREF_KEY_TOKEN = "PREF_KEY_TOKEN"
+    }
+}
