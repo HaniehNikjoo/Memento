b919497adf8836957525d9026f659bfdd713fc6c
hanieh nikjoo <hnik22@gmailo.com>
2020-01-24 14:25:59 +0330

    Updated ViewModelModule with add SplashViewModel

--- app/src/main/java/com/example/myapplication/di/ViewModelModule.kt
+++ app/src/main/java/com/example/myapplication/di/ViewModelModule.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.di
 import com.example.myapplication.ui.dashboard.DashboardViewModel
 import com.example.myapplication.ui.home.HomeViewModel
 import com.example.myapplication.ui.notifications.NotificationsViewModel
+import com.example.myapplication.ui.splash.SplashViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -10,4 +11,5 @@ val viewModelModule = module {
         viewModel { DashboardViewModel() }
         viewModel { HomeViewModel() }
         viewModel { NotificationsViewModel() }
+        viewModel { SplashViewModel() }
 }
